41698827f14617f926f50ab1ed8efe859450e4a6
Patrick Camacho <patrick@daftdevelopers.com>
2018-03-05 18:20:21 -0700

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# auto-publish
-Automatically increment and publish module versions to NPM
+# Auto-publish
+Automatically increment and publish module versions to NPM. Uses existing published versions to detect next version (rather than depending on the `package.json` file being updated).
+
+# Install
+```sh
+yarn add -D continuous-publication
+# or
+npm install --save-dev continuous-publication
+```
+
+# Usage
+
+```sh
+$ cp -h
+
+cp [bump]
+
+Increment module version and publish
+
+Positionals:
+  bump                   [choices: "patch", "minor", "major"] [default: "patch"]
+
+Options:
+  --client                   [string] [choices: "yarn", "npm"] [default: "yarn"]
+  --tag       Tag to use when publishing            [string] [default: "latest"]
+  --dry       Dry run (do not publish)                [boolean] [default: false]
+  -h, --help  Show help                                                [boolean]
+```
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+#! /usr/bin/env node
+
 const findup = require('find-up');
 const execa = require('execa');
 const semver = require('semver');
+const yon = require('yarn-or-npm');
 
-async function execute(client = 'yarn', bump = 'patch', tag = 'latest') {
+async function execute(client, bump, tag, options = {}) {
   const packagePath = await findup('package.json');
   if (!packagePath) throw new Error('package.json could not be found');
 
@@ -13,20 +16,85 @@ async function execute(client = 'yarn', bump = 'patch', tag = 'latest') {
 
   const major = semver.major(info.version);
   const minor = semver.minor(info.version);
+  const patch = semver.patch(info.version);
 
-  const lastVersion = info.versions.sort(semver.rcompare).filter(version => {
-    return semver.major(version) === major && semver.minor(version) === minor;
-  })[0];
+  const sortedVersions = info.versions.sort(semver.rcompare);
 
-  const nextVersion = lastVersion
-    ? semver.inc(lastVersion, bump)
-    : info.version;
+  let latestRelevantVersion;
+  switch (bump) {
+    case 'patch':
+      latestRelevantVersion = sortedVersions.filter(
+        version =>
+          semver.major(version) === major && semver.minor(version) === minor
+      )[0];
+      break;
+    case 'minor':
+      latestRelevantVersion = sortedVersions.filter(
+        version => semver.major(version) === major
+      )[0];
+      break;
+    case 'major':
+      latestRelevantVersion = sortedVersions[0];
+      break;
+    default:
+      throw new Error('Illegal bump type provided');
+  }
 
-  await execa(client, ['version', '--new-version', nextVersion], {
-    stdio: 'inherit',
-  });
+  const nextVersion = semver.inc(
+    latestRelevantVersion ? latestRelevantVersion : version,
+    bump
+  );
 
-  await execa(client, ['publish', '--tag', tag]);
+  const args = ['publish', '--new-version', nextVersion, '--tag', tag];
+
+  if (options.dry) {
+    console.log(`Next Version: ${nextVersion}`);
+    console.log(`Publish Command: ${[client].concat(args).join(' ')}`);
+  } else {
+    await execa(client, args, { stdio: 'inherit' });
+  }
 }
 
-execute(undefined, undefined, 'alpha');
+if (require.main === module) {
+  const yargs = require('yargs');
+  const argv = yargs
+    .command(
+      ['* [bump]'],
+      'Increment module version and publish',
+      localYargs => {
+        localYargs.positional('bump', {
+          default: 'patch',
+          choices: ['patch', 'minor', 'major'],
+        });
+      }
+    )
+    .options({
+      client: {
+        type: 'string',
+        choices: ['yarn', 'npm'],
+        default: yon(),
+      },
+      tag: {
+        type: 'string',
+        description: 'Tag to use when publishing',
+        default: 'latest',
+      },
+      dry: {
+        type: 'boolean',
+        description: 'Dry run (do not publish)',
+        default: false,
+      },
+    })
+    .version(false)
+    .strict()
+    .alias('h', 'help')
+    .help('help').argv;
+
+  try {
+    const { client, bump, tag, ...options } = argv;
+    execute(client, bump, tag, options);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
